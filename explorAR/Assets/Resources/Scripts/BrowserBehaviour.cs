4f844bda445bcc944f384385faa9a6c3e8fca849
pKehrbusch <philipp.kehrbusch@live.de>
2016-04-16 17:43:59 +0200

    finnished browser with dummies

---
+++ explorAR/Assets/Resources/Scripts/BrowserBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Explorar;
+
+public class BrowserBehaviour : MonoBehaviour, IBrowser {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ShowNext() {
+		gameObject.transform.Rotate (new Vector3 (0, 0, 360 / 5));
+	}
+
+	public void ShowPrevious() {
+		gameObject.transform.Rotate (new Vector3 (0, 0, -360 / 5));
+	}
+}
---
+++ explorAR/Assets/Resources/Scripts/IBrowser.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Explorar {
+	public interface IBrowser {
+
+		void ShowNext();
+		void ShowPrevious();
+	}
+}
