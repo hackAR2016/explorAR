9084088a5c7f76d044591db103b1a58ccdd5f411
pKehrbusch <philipp.kehrbusch@live.de>
2016-04-16 19:02:46 +0200

    changed browser to linear

--- explorAR/Assets/Resources/Scripts/BrowserBehaviour.cs
+++ explorAR/Assets/Resources/Scripts/BrowserBehaviour.cs
@@ -4,9 +4,19 @@ using Explorar;
 
 public class BrowserBehaviour : MonoBehaviour, IBrowser {
 
+	private int houseCount = 5;
+	private int currentHouseIndex = 0;
+	private Vector3 currentHousePos = new Vector3 (0, 0, 0);
+	private Vector3 nextHousePos = new Vector3 (-10, 0, 0);
+	private Vector3 prevHousePos = new Vector3 (10, 0, 0);
+
+	private GameObject currentHouse;
+	private GameObject prevHouse;
+	private GameObject nextHouse;
+
 	// Use this for initialization
 	void Start () {
-	
+		updateHouses ();
 	}
 	
 	// Update is called once per frame
@@ -15,10 +25,38 @@ public class BrowserBehaviour : MonoBehaviour, IBrowser {
 	}
 
 	public void ShowNext() {
-		gameObject.transform.Rotate (new Vector3 (0, 0, 360 / 5));
+		currentHouseIndex = (currentHouseIndex + 1) % houseCount;
+		updateHouses ();
 	}
 
 	public void ShowPrevious() {
-		gameObject.transform.Rotate (new Vector3 (0, 0, -360 / 5));
+		currentHouseIndex = currentHouseIndex -1;
+		if (currentHouseIndex == -1)
+			currentHouseIndex = houseCount - 1;
+		updateHouses ();
+	}
+
+	private void updateHouses() {
+		Destroy (prevHouse);
+		Destroy (currentHouse);
+		Destroy (nextHouse);
+
+		int prev = currentHouseIndex - 1;
+		if (prev == -1)
+			prev = houseCount - 1;
+		prev++;
+		int next = ((currentHouseIndex + 1) % houseCount) + 1;
+		
+		prevHouse = GameObject.Instantiate (Resources.Load<GameObject> ("Prefabs/House" + prev));
+		currentHouse = GameObject.Instantiate (Resources.Load<GameObject> ("Prefabs/House" + (currentHouseIndex + 1)));
+		nextHouse = GameObject.Instantiate (Resources.Load<GameObject> ("Prefabs/House" + next));
+
+		prevHouse.transform.parent = gameObject.transform;
+		currentHouse.transform.parent = gameObject.transform;
+		nextHouse.transform.parent = gameObject.transform;
+
+		prevHouse.transform.localPosition = prevHousePos;
+		currentHouse.transform.localPosition = currentHousePos;
+		nextHouse.transform.localPosition = nextHousePos;
 	}
 }
