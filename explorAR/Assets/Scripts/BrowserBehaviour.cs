5844679a260a041d6e32019bf0fc6553aaa26640
pKehrbusch <philipp.kehrbusch@live.de>
2016-04-16 16:28:46 +0200

    rotate browser

--- explorAR/Assets/Scripts/BrowserBehaviour.cs
+++ explorAR/Assets/Scripts/BrowserBehaviour.cs
@@ -4,6 +4,8 @@ using Explorar;
 
 public class BrowserBehaviour : MonoBehaviour, IBrowser {
 
+	public int ObjectCount = 5;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -15,10 +17,10 @@ public class BrowserBehaviour : MonoBehaviour, IBrowser {
 	}
 
 	public void ShowNext() {
-
+		gameObject.transform.Rotate (new Vector3 (0, 360/ObjectCount, 0));
 	}
 
 	public void ShowPrevious() {
-		
+		gameObject.transform.Rotate (new Vector3 (0, -360/ObjectCount, 0));
 	}
 }
