e7059aaec3dd9f824bc4a1407446c5543fd6416b
pKehrbusch <philipp.kehrbusch@live.de>
2016-04-16 16:14:28 +0200

    added resources folder

---
+++ explorAR/Assets/Scripts/BrowserLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Explorar {
+	public class BrowserLoader {
+
+		public void ShowBrowser() {
+			GameObject.Instantiate (Resources.Load<GameObject> (""));
+		}
+	}
+}
