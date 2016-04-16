962490ad3c5f5237b16003d267d74f77894a5224
pKehrbusch <philipp.kehrbusch@live.de>
2016-04-16 20:45:55 +0200

    modified object browser

--- explorAR/Assets/Resources/Scripts/BrowserBehaviour.cs
+++ explorAR/Assets/Resources/Scripts/BrowserBehaviour.cs
@@ -6,57 +6,46 @@ public class BrowserBehaviour : MonoBehaviour, IBrowser {
 
 	private int houseCount = 5;
 	private int currentHouseIndex = 0;
-	private Vector3 currentHousePos = new Vector3 (0, 0, 0);
-	private Vector3 nextHousePos = new Vector3 (-10, 0, 0);
-	private Vector3 prevHousePos = new Vector3 (10, 0, 0);
 
-	private GameObject currentHouse;
-	private GameObject prevHouse;
-	private GameObject nextHouse;
+	public float smallScale = 0.5f;
+	public float largeScale = 1f;
+
+	private GameObject[] houses = new GameObject[5];
 
 	// Use this for initialization
 	void Start () {
-		updateHouses ();
+		houses[0] = GameObject.Find ("FrameMarker0/ObjectBrowser/House1");
+		houses[1] = GameObject.Find ("FrameMarker0/ObjectBrowser/House2");
+		houses[2] = GameObject.Find ("FrameMarker0/ObjectBrowser/House3");
+		houses[3] = GameObject.Find ("FrameMarker0/ObjectBrowser/House4");
+		houses[4] = GameObject.Find ("FrameMarker0/ObjectBrowser/House5");
+
+		selectHouse (currentHouseIndex);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
 	public void ShowNext() {
 		currentHouseIndex = (currentHouseIndex + 1) % houseCount;
-		updateHouses ();
+		selectHouse (currentHouseIndex);
 	}
 
 	public void ShowPrevious() {
 		currentHouseIndex = currentHouseIndex -1;
 		if (currentHouseIndex == -1)
-			currentHouseIndex = houseCount - 1;
-		updateHouses ();
+			currentHouseIndex = houseCount = 4;
+		selectHouse (currentHouseIndex);
 	}
 
-	private void updateHouses() {
-		Destroy (prevHouse);
-		Destroy (currentHouse);
-		Destroy (nextHouse);
-
-		int prev = currentHouseIndex - 1;
-		if (prev == -1)
-			prev = houseCount - 1;
-		prev++;
-		int next = ((currentHouseIndex + 1) % houseCount) + 1;
-		
-		prevHouse = GameObject.Instantiate (Resources.Load<GameObject> ("Prefabs/House" + prev));
-		currentHouse = GameObject.Instantiate (Resources.Load<GameObject> ("Prefabs/House" + (currentHouseIndex + 1)));
-		nextHouse = GameObject.Instantiate (Resources.Load<GameObject> ("Prefabs/House" + next));
-
-		prevHouse.transform.parent = gameObject.transform;
-		currentHouse.transform.parent = gameObject.transform;
-		nextHouse.transform.parent = gameObject.transform;
-
-		prevHouse.transform.localPosition = prevHousePos;
-		currentHouse.transform.localPosition = currentHousePos;
-		nextHouse.transform.localPosition = nextHousePos;
+	private void selectHouse(int index) {
+		for(int i = 0; i < houseCount; i++) {
+			if (i != index) {
+				houses [i].transform.localScale = new Vector3(smallScale, smallScale, smallScale);
+			}
+		}
+		houses [index].transform.localScale = new Vector3(largeScale, largeScale, largeScale);
 	}
 }
